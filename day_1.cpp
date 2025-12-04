b637cff3434d0a055af7c4252bf3c2030aa4298a
Ifra-Malik <malikifra4u@gmail.com>
2025-12-04 21:40:21 +0500

    day_2

---
+++ day_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+namespace ifra{
+    string name="maryam";
+}
+int main(){
+
+    // typedef bool one;
+    // typedef string ifra;
+    // ifra name="malik";
+    // one two=true;
+    cout<<ifra::name<<endl;
+    // cout<<two<<endl;
+    return 0;
+
+}
